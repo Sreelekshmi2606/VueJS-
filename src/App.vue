721dcb840bb1a8dc3bed106ee38ce68cd9adf8ab
Sreelekshmi2606 <105813518+Sreelekshmi2606@users.noreply.github.com>
2022-06-14 09:11:53 +0530

    Menubar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <CordiAn />
-  </div>
+  <DockMenu :items="items">
+  </DockMenu>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import CordiAn from './components/cordian.vue'
+import { DockMenu } from "vue-dock-menu";
+import "vue-dock-menu/dist/vue-dock-menu.css";
+
+export default {
+  name: "ExampLe",
+  components: {
+    DockMenu
+  },
+  data() {
+    return {
+      items: [
+        {
+          name: "Home",
+          menu: [{ name: "About" }, { name: "News", }, { name: "Gallary" }]
+        },
+        {
+          name: "Products",
+          menu: [{ name: "Vegetables" }, {
+            name: "Fruits",
+            menu: [{ name: "Orange" }, { name: "Apple" }]
+          }, { name: "Sweets" }]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
+
+
+
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    CordiAn
+    HelloWorld
   }
 }
 </script>
@@ -28,4 +61,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
